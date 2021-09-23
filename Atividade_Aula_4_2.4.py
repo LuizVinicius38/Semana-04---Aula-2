9000f01c88b795cce07d408bb0367b60faf31dff
LuizVinicius38 <89944844+LuizVinicius38@users.noreply.github.com>
2021-09-23 13:32:07 -0300

    Delete Atividade_Aula_4_2.4.py

--- Atividade_Aula_4_2.4.py
+++
@@ -1,8 +0,0 @@
-horas = int(input(""))
-minutos = int(input(""))
-segundos = int(input(""))
-totalHorasEmSeg = horas * 3600
-totalMinutosEmSeg = minutos * 60
-totalSeg = totalHorasEmSeg + totalMinutosEmSeg + segundos
-print(totalSeg)
-
